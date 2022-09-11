722359a99ee2eca6ad5a38534d3b86092c3221b9
thesabbirahmed <112713338+thesabbirahmed@users.noreply.github.com>
2022-09-11 12:49:10 +0600

    d

---
+++ string.js
@@ -0,0 +1,10 @@
+const firstName='Justin';
+const lastName='TimberLake';
+const fullName=firstName+' '+lastName;
+const fullName2=`${firstName} ${lastName} is a good boy.`;
+
+const multiLine=`${firstName} ${lastName}
+is a boy of 
+honor.`
+
+console.log(multiLine);
